9decd24195aab000005de18b085eb0c8faf10c39
Mashell Pervaiz <mpervaiz@hawk.iit.edu>
2025-04-29 23:33:35 -0500

    initializing google maps
    javascript

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,16 @@
+// Google Map Initialization
+function initMap() {
+  const iitLocation = { lat: 41.8349, lng: -87.6270 }; // IIT Chicago coordinates
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: iitLocation,
+  });
+
+
+
+
+
+
 // JavaScript for the Image Slider
 
 // Get all required elements
